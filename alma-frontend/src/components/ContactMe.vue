9994e72fe58542a981a20f3f20270642d6b39330
Ale Gómez <gom20347@uvg.edu.gt>
2022-04-05 23:55:34 -0600

    added spacing between buttons/added bold text

--- alma-frontend/src/components/ContactMe.vue
+++ alma-frontend/src/components/ContactMe.vue
@@ -3,9 +3,7 @@
     <v-container>
       <v-row>
         <v-col>
-          <p class="text-center mb-0 font-weight-bold">
-            Comunícate conmigo
-          </p>
+          <p class="text-center mb-0 font-weight-bold">Comunícate conmigo</p>
         </v-col>
       </v-row>
     </v-container>
@@ -14,14 +12,14 @@
       <v-row>
         <v-col cols="12">
           <v-card color="#F8F6FF" flat>
-            <section class="d-flex flex-column py-5 justify-center align-center">
+            <section
+              class="d-flex flex-column py-5 justify-center align-center"
+            >
               <section>
                 <!-- <span class="display-4">
                   📱
                 </span> -->
-                <span class="display-4">
-                  📞
-                </span>
+                <span class="display-4"> 📞 </span>
               </section>
 
               <section class="mt-3">
@@ -37,8 +35,12 @@
               </section>
 
               <section class="mt-3">
-                <v-btn rounded color="#FFE6B7" elevation="0"
-                    href="tel:1801-0054-321">
+                <v-btn
+                  rounded
+                  color="#FFE6B7"
+                  elevation="0"
+                  href="tel:1801-0054-321"
+                >
                   <v-icon>mdi-phone-outline</v-icon>
                   1801-0054-321
                 </v-btn>
@@ -53,21 +55,25 @@
       <v-row>
         <v-col cols="12">
           <v-card color="#DFF5EB" flat>
-            <section class="d-flex flex-column py-5 justify-center align-center">
+            <section
+              class="d-flex flex-column py-5 justify-center align-center"
+            >
               <section>
                 <!-- <span class="display-4">
                   𓀊
                 </span> -->
 
                 <span class="display-4">
-                  <v-img :src="require('@/assets/icons/svg-contact-me-whatsapp-icon.svg')"></v-img>
-                </span>  
+                  <v-img
+                    :src="
+                      require('@/assets/icons/svg-contact-me-whatsapp-icon.svg')
+                    "
+                  ></v-img>
+                </span>
               </section>
 
               <section class="mt-3">
-                <span class="font-weight-bold">
-                  Escríbenos por WhatsApp
-                </span>
+                <span class="font-weight-bold"> Escríbenos por WhatsApp </span>
               </section>
 
               <section class="mt-3 text-center">
@@ -77,8 +83,12 @@
               </section>
 
               <section class="mt-3">
-                <v-btn rounded color="#FFE6B7" elevation="0"
-                    href="https://api.whatsapp.com/send?phone=50224144321&text=Preg%C3%BAntale%20a%20ALMA">
+                <v-btn
+                  rounded
+                  color="#FFE6B7"
+                  elevation="0"
+                  href="https://api.whatsapp.com/send?phone=50224144321&text=Preg%C3%BAntale%20a%20ALMA"
+                >
                   <v-icon>mdi-whatsapp</v-icon>
                   Whatsapp
                 </v-btn>
@@ -93,15 +103,19 @@
       <v-row>
         <v-col cols="12">
           <v-card color="#FFEBE8" flat>
-            <section class="d-flex flex-column py-5 justify-center align-center">
+            <section
+              class="d-flex flex-column py-5 justify-center align-center"
+            >
               <section>
                 <!-- <span class="display-4">
                   ❤️
                 </span> -->
 
                 <span class="display-4">
-                  <v-img :src="require('@/assets/icons/svg-contact-me-heart.svg')"></v-img>
-                </span>  
+                  <v-img
+                    :src="require('@/assets/icons/svg-contact-me-heart.svg')"
+                  ></v-img>
+                </span>
               </section>
 
               <section class="mt-3">
@@ -112,22 +126,36 @@
 
               <section class="mt-3 text-center">
                 <span class="font-weight-light">
-                  Te podrás enterar de muchas cosas relacionas con COVID y además pregúntame lo que quieras
+                  Te podrás enterar de muchas cosas relacionas con COVID y
+                  además pregúntame lo que quieras
                 </span>
               </section>
 
               <section class="mt-3" style="width: 100%">
                 <section class="d-flex justify-space-around">
-                  <v-btn rounded color="#FFE6B7" elevation="0"
-                      href="https://twitter.com/alma_responde">
-                    <v-icon>mdi-twitter</v-icon>
-                    twitter
+                  <v-btn
+                    rounded
+                    color="#FFE6B7"
+                    elevation="0"
+                    href="https://twitter.com/alma_responde"
+                  >
+                    <v-icon style="margin-right: 6px">mdi-twitter</v-icon>
+
+                    <span style="text-transform: none; font-weight: bold"
+                      >Twitter</span
+                    >
                   </v-btn>
 
-                  <v-btn rounded color="#FFE6B7" elevation="0"
-                      href="https://www.facebook.com/chatbotALMA">
-                    <v-icon>mdi-facebook</v-icon>
-                    facebook
+                  <v-btn
+                    rounded
+                    color="#FFE6B7"
+                    elevation="0"
+                    href="https://www.facebook.com/chatbotALMA"
+                  >
+                    <v-icon style="margin-right: 6px">mdi-facebook</v-icon>
+                    <span style="text-transform: none; font-weight: bold"
+                      >Facebook</span
+                    >
                   </v-btn>
                 </section>
               </section>
