e26aedf009fde46575907422eba5e96da4340d37
Ale Gómez <gom20347@uvg.edu.gt>
2022-04-09 22:16:31 -0600

    created social media component

---
+++ alma-frontend/src/components/SocialMedia.vue
@@ -0,0 +1,51 @@
+<template>
+  <section>
+    <v-container>
+      <v-row>
+        <v-col cols="12">
+          <v-card color="#FFEBE8" flat class="rounded-lg">
+            <section class="d-flex flex-column py-5 justify-center">
+              <section class="mt-0 ml-15">
+                <h2 class="font-weight-bold">
+                  Sigueme por nuestras redes sociales
+                </h2>
+              </section>
+
+              <section class="mt-3 text-jusfify ml-15">
+                <span class="font-weight-light">
+                  Te podrás enterar de muchas cosas relacionas con COVID y
+                  además pregúntame lo que quieras.
+                </span>
+              </section>
+
+              <section class="mt-3" style="width: 100%">
+                <section class="d-flex justify-space-around">
+                  <v-btn
+                    rounded
+                    color="#FFE6B7"
+                    elevation="0"
+                    href="https://twitter.com/alma_responde"
+                  >
+                    <v-icon style="margin-right: 6px">mdi-twitter</v-icon>
+
+                    <span style="text-transform: none">Twitter</span>
+                  </v-btn>
+
+                  <v-btn
+                    rounded
+                    color="#FFE6B7"
+                    elevation="0"
+                    href="https://www.facebook.com/chatbotALMA"
+                  >
+                    <v-icon style="margin-right: 6px">mdi-facebook</v-icon>
+                    <span style="text-transform: none">Facebook</span>
+                  </v-btn>
+                </section>
+              </section>
+            </section>
+          </v-card>
+        </v-col>
+      </v-row>
+    </v-container>
+  </section>
+</template>
