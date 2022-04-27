287f14970264042c7b0620437ede5ad8593f598c
Ale Gómez <gom20347@uvg.edu.gt>
2022-04-26 21:36:34 -0600

    fixed margins, added ":" between day and hour

--- alma-frontend/src/components/CenterDetail.vue
+++ alma-frontend/src/components/CenterDetail.vue
@@ -3,32 +3,41 @@
     <v-list two-line>
       <v-list-item>
         <v-list-item-avatar>
-          <v-icon>
-            mdi-clock-outline
-          </v-icon>
+          <v-icon> mdi-clock-outline </v-icon>
         </v-list-item-avatar>
 
         <v-list-item-content>
           <v-list-item-title class="font-weight-bold">
-            Horario de Atencion
+            Horario de Atención
           </v-list-item-title>
 
           <v-list-item-subtitle
-              v-if="selected.schedule.week"
-              class="font-weight-medium">
+            v-if="selected.schedule.week"
+            class="font-weight-medium"
+          >
             {{ getSchedule }} {{ selected.schedule.week }}
           </v-list-item-subtitle>
 
           <v-list-item-subtitle
-              v-if="selected.schedule.weekend"
-              class="font-weight-medium">
-            Sab {{ selected.days.saturday ? selected.schedule.weekend : 'No disponible' }}
+            v-if="selected.schedule.weekend"
+            class="font-weight-medium"
+          >
+            Sab:
+            {{
+              selected.days.saturday
+                ? selected.schedule.weekend
+                : "No disponible"
+            }}
           </v-list-item-subtitle>
 
           <v-list-item-subtitle
-              v-if="selected.schedule.weekend"
-              class="font-weight-medium">
-            Dom {{ selected.days.sunday ? selected.schedule.weekend : 'No disponible' }}
+            v-if="selected.schedule.weekend"
+            class="font-weight-medium"
+          >
+            Dom:
+            {{
+              selected.days.sunday ? selected.schedule.weekend : "No disponible"
+            }}
           </v-list-item-subtitle>
         </v-list-item-content>
 
@@ -41,9 +50,7 @@
 
       <v-list-item>
         <v-list-item-avatar>
-          <v-icon>
-            mdi-checkbox-marked-circle-outline
-          </v-icon>
+          <v-icon> mdi-checkbox-marked-circle-outline </v-icon>
         </v-list-item-avatar>
 
         <v-list-item-content>
@@ -51,12 +58,8 @@
             Vacunas Disponibles
           </v-list-item-title>
 
-          <v-list-item-subtitle
-              v-for="(item, i) in formatedVaccines"
-              :key="i">
-            <span class="font-weight-bold">
-              {{ item.vaccine }}:
-            </span>
+          <v-list-item-subtitle v-for="(item, i) in formatedVaccines" :key="i">
+            <span class="font-weight-bold"> {{ item.vaccine }}: </span>
 
             <span class="font-weight-medium">
               {{ item.detail }}
@@ -73,14 +76,12 @@
 
       <v-list-item>
         <v-list-item-avatar>
-          <v-icon>
-            mdi-map-marker-outline
-          </v-icon>
+          <v-icon> mdi-map-marker-outline </v-icon>
         </v-list-item-avatar>
 
         <v-list-item-content>
           <v-list-item-title class="font-weight-bold">
-            Direccion
+            Dirección
           </v-list-item-title>
 
           <v-list-item-subtitle class="font-weight-medium">
@@ -88,16 +89,13 @@
           </v-list-item-subtitle>
 
           <div>
-            <v-btn color="#65CBF9" rounded
-                @click="openMaps">
+            <v-btn color="#65CBF9" rounded class="mt-2 mb-2" @click="openMaps">
               Abrir en Waze
             </v-btn>
           </div>
 
           <div class="mt-2">
-            <v-btn rounded @click="onShare">
-              Compartir
-            </v-btn>
+            <v-btn rounded @click="onShare" class="mb-2"> Compartir </v-btn>
           </div>
         </v-list-item-content>
 
@@ -113,9 +111,7 @@
       <v-list>
         <v-list-item>
           <v-list-item-avatar>
-            <v-icon>
-              mdi-comment-text-multiple-outline
-            </v-icon>
+            <v-icon> mdi-comment-text-multiple-outline </v-icon>
           </v-list-item-avatar>
 
           <v-list-item-content>
@@ -136,30 +132,29 @@
         <v-row>
           <v-col cols="12">
             <v-card color="#F3F3F3">
-              <v-card-title class="caption" style="color: #989898;">
+              <v-card-title class="caption" style="color: #989898">
                 <span>Febrero 2, 2022</span>
                 <v-spacer></v-spacer>
                 <span>3:42pm</span>
               </v-card-title>
 
               <v-card-text class="pb-0">
-                Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolorum debitis aspernatur quis magni placeat incidunt rem accusantium fuga accusamus voluptas? Ipsa reiciendis nam iure. Libero voluptatem mollitia placeat repellat rerum?
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolorum
+                debitis aspernatur quis magni placeat incidunt rem accusantium
+                fuga accusamus voluptas? Ipsa reiciendis nam iure. Libero
+                voluptatem mollitia placeat repellat rerum?
               </v-card-text>
 
               <v-card-actions>
                 <v-btn text>
-                  <span class="text-decoration-underline">
-                    Ver Mas
-                  </span>
+                  <span class="text-decoration-underline"> Ver Mas </span>
                 </v-btn>
               </v-card-actions>
             </v-card>
           </v-col>
 
           <v-col cols="12" class="d-flex justify-center">
-            <v-btn color="#FFE6B7" rounded>
-              Escribir Comentario
-            </v-btn>
+            <v-btn color="#FFE6B7" rounded> Escribir Comentario </v-btn>
           </v-col>
         </v-row>
       </v-container>
@@ -168,51 +163,49 @@
 </template>
 
 <script>
-import { mapGetters } from 'vuex'
+import { mapGetters } from "vuex";
 
 export default {
   computed: {
-    ...mapGetters(['selected']),
+    ...mapGetters(["selected"]),
 
     getSchedule() {
-      let start = ''
-      let end = ''
+      let start = "";
+      let end = "";
 
       if (this.selected.days.monday) {
-        start = 'Lun'
+        start = "Lun";
       } else if (this.selected.days.tuesday) {
-        start = 'Mar'
+        start = "Mar";
       }
 
       if (this.selected.days.friday) {
-        end = 'Vie'
+        end = "Vie";
       } else if (this.selected.days.thursday) {
-        end = 'Jue'
+        end = "Jue";
       }
 
-      return `${start} - ${end}`
+      return `${start} - ${end}:`;
     },
 
     formatedVaccines() {
-      const vaccines = []
+      const vaccines = [];
 
-      Object.entries(this.selected.vaccines)
-        .forEach(([vaccine, dose]) => {
-          Object.entries(dose)
-            .forEach(([doseKey, doseData]) => {
-              doseData.forEach(req => {
-                if (req.available) {
-                  vaccines.push({
-                    vaccine: vaccine,
-                    detail: `${this.formatDose(doseKey)} / ${req.group}`
-                  })
-                }
-              })
-            })
-        })
+      Object.entries(this.selected.vaccines).forEach(([vaccine, dose]) => {
+        Object.entries(dose).forEach(([doseKey, doseData]) => {
+          doseData.forEach((req) => {
+            if (req.available) {
+              vaccines.push({
+                vaccine: vaccine,
+                detail: `${this.formatDose(doseKey)} / ${req.group}`,
+              });
+            }
+          });
+        });
+      });
 
-      return vaccines
-    }
+      return vaccines;
+    },
   },
 
   methods: {
@@ -222,18 +215,21 @@ export default {
 
     formatDose(dose) {
       switch (dose) {
-        case 'PRIMERA': return '1era'
-        case 'SEGUNDA': return '2nda'
-        default: return dose
+        case "PRIMERA":
+          return "1era";
+        case "SEGUNDA":
+          return "2nda";
+        default:
+          return dose;
       }
     },
 
     async onShare() {
       await navigator.share({
-        title: 'VacunasGT Center',
+        title: "VacunasGT Center",
         url: window.location.href,
-      })
-    }
-  }
-}
+      });
+    },
+  },
+};
 </script>
