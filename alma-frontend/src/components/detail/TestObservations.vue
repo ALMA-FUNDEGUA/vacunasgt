5fb602d4ceb7a25e39689c687d818930bdac1e62
Ale Gómez <gom20347@uvg.edu.gt>
2022-08-08 09:41:48 -0600

    changed covid test schedule format

--- alma-frontend/src/components/detail/TestObservations.vue
+++ alma-frontend/src/components/detail/TestObservations.vue
@@ -8,6 +8,25 @@
           {{ item.tests[0].observations }}
         </span>
 
+        <v-col cols="8" class="pa-0 pb-2 caption">
+          <span class="font-weight-bold"> </span>
+        </v-col>
+        <v-row
+          v-for="(item, i) in testsAvailable.slice(0, 1)"
+          :key="i"
+          class="pa-0 pb-5 pt-2 caption"
+        >
+          <span class="font-weight-medium" v-if="item.specificSchedule != null">
+            <span class="font-weight-bold"> Horario:</span>
+            {{ item.specificSchedule }}
+          </span>
+
+          <span class="font-weight-medium" v-else>
+            <span class="font-weight-bold"> Horario:</span>
+            Por confirmar
+          </span>
+        </v-row>
+
         <v-else></v-else>
         <div class="mt-1">
           <v-btn rounded @click="onShare" class="mb-2"> Compartir </v-btn>
@@ -27,6 +46,11 @@ export default {
     InformationSection,
   },
 
+  computed: {
+    testsAvailable() {
+      return this.item.tests.filter((item) => item.available)
+    },
+  },
   methods: {
     verifyObservation() {
       return this.item.tests[0].observations
