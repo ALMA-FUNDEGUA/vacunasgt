cd90c11ce1550dde7290fc061e52791784ba780d
Ale Gómez <gom20347@uvg.edu.gt>
2022-08-18 21:49:32 -0600

    changed text on vaccine centers

--- alma-frontend/src/components/MobileCenterList.vue
+++ alma-frontend/src/components/MobileCenterList.vue
@@ -3,24 +3,38 @@
     <search-bar-mobile v-model="search"></search-bar-mobile>
 
     <v-card flat class="text-center">
-      <v-toolbar flat color=#E4DFFF>
+      <v-toolbar flat color="#E4DFFF">
         <v-btn icon large link to="/">
           <v-icon>mdi-chevron-left</v-icon>
         </v-btn>
 
-        <span style="font-family: 'Poppins'; font-style: normal; font-weight: 700; font-size: 18px; line-height: 27px;">
+        <span
+          style="
+            font-family: 'Poppins';
+            font-style: normal;
+            font-weight: 700;
+            font-size: 18px;
+            line-height: 27px;
+          "
+        >
           Vacunas
         </span>
-        
-        <v-spacer/>
-        
+
+        <v-spacer />
+
         <v-btn icon link to="/filters">
-          <v-icon>
-            mdi-filter-variant
-          </v-icon>
+          <v-icon> mdi-filter-variant </v-icon>
         </v-btn>
 
-        <span style="font-family: 'Poppins'; font-style: normal; font-weight: 700; font-size: 14px; line-height: 21px;">
+        <span
+          style="
+            font-family: 'Poppins';
+            font-style: normal;
+            font-weight: 700;
+            font-size: 14px;
+            line-height: 21px;
+          "
+        >
           Filtros
         </span>
       </v-toolbar>
@@ -29,71 +43,116 @@
       <map-filter></map-filter>
 
       <v-data-iterator
-          :items="centers"
-          :search="search"
-          :loading="loading"
-          :items-per-page="-1"
-          hide-default-footer>
+        :items="centers"
+        :search="search"
+        :loading="loading"
+        :items-per-page="-1"
+        hide-default-footer
+      >
         <template #header="{ items }">
-          <v-card-title class="justify-center" style="font-family: 'Poppins'; font-style: normal; font-weight: 700; font-size: 18px; line-height: 27px;">
+          <v-card-title
+            class="justify-center"
+            style="
+              font-family: 'Poppins';
+              font-style: normal;
+              font-weight: 700;
+              font-size: 18px;
+              line-height: 27px;
+            "
+          >
             Resultados ({{ items.length }})
           </v-card-title>
-          
+
           <v-divider></v-divider>
         </template>
 
         <template #default="{ items }">
-          <v-card v-for="(center, i) in items" :key="`center-${i}`"
-              @click="onSelect(center.name)">
-            <v-card-title style="font-family: 'Poppins'; font-style: normal; font-weight: 700; font-size: 18px; line-height: 27px; color: #000000; text-align:left; word-break: break-word;">
+          <v-card
+            v-for="(center, i) in items"
+            :key="`center-${i}`"
+            @click="onSelect(center.name)"
+          >
+            <v-card-title
+              style="
+                font-family: 'Poppins';
+                font-style: normal;
+                font-weight: 700;
+                font-size: 18px;
+                line-height: 27px;
+                color: #000000;
+                text-align: left;
+                word-break: break-word;
+              "
+            >
               <v-row dense>
                 <v-col cols="11">
-                  Lugar: {{ center.name }}
+                  {{ center.name }}
                 </v-col>
 
                 <v-col cols="1">
-                  <v-icon large color=#A698FF>
-                    mdi-chevron-right
-                  </v-icon>
+                  <v-icon large color="#A698FF"> mdi-chevron-right </v-icon>
                 </v-col>
               </v-row>
             </v-card-title>
-            
-            <v-card-subtitle class="pb-1" style="text-align:left font-family: 'Poppins'; font-style: normal; font-weight: 400; font-size: 12px; line-height: 22px; color: #848282;"> 
+
+            <v-card-subtitle
+              class="pb-1"
+              style="text-align:left font-family: 'Poppins'; font-style: normal; font-weight: 400; font-size: 12px; line-height: 22px; color: #848282;"
+            >
               Ultima actualización de datos:
             </v-card-subtitle>
-            
-            <v-card-text style="font-family: 'Poppins'; font-style: normal; font-weight: 400; font-size: 14px; line-height: 22px;">
+
+            <v-card-text
+              style="
+                font-family: 'Poppins';
+                font-style: normal;
+                font-weight: 400;
+                font-size: 14px;
+                line-height: 22px;
+              "
+            >
               <v-row dense>
                 <v-col cols="1">
-                  <v-icon size=25 top>
-                    mdi-clock-outline
-                  </v-icon>
+                  <v-icon size="25" top> mdi-clock-outline </v-icon>
                 </v-col>
 
-                <v-col cols="11" class="d-flex flex-column justify-end black--text text-left">
+                <v-col
+                  cols="11"
+                  class="d-flex flex-column justify-end black--text text-left"
+                >
                   <div>
-                    <span v-if="center.schedule.week">      
+                    <span v-if="center.schedule.week">
                       Lun - Vie > {{ center.schedule.week }}
-                      <br>
+                      <br />
                     </span>
 
                     <span v-if="center.schedule.weekend">
-                      Sab > {{ center.days.saturday ? center.schedule.weekend : "No disponible" }}
+                      Sab >
+                      {{
+                        center.days.saturday
+                          ? center.schedule.weekend
+                          : 'No disponible'
+                      }}
                       <br />
 
-                      Dom > {{ center.days.sunday ? center.schedule.weekend : "No disponible"}}
+                      Dom >
+                      {{
+                        center.days.sunday
+                          ? center.schedule.weekend
+                          : 'No disponible'
+                      }}
                     </span>
                   </div>
                 </v-col>
 
                 <v-col cols="1">
-                  <v-icon size=25 top>
-                    mdi-map-marker-outline
-                  </v-icon>
+                  <v-icon size="25" top> mdi-map-marker-outline </v-icon>
                 </v-col>
 
-                <v-col cols="11" class="d-flex flex-column justify-end black--text text-left">
+                <v-col
+                  cols="11"
+                  class="d-flex flex-column justify-end black--text text-left"
+                >
                   {{ center.address }}
                 </v-col>
               </v-row>
@@ -143,25 +202,27 @@
       <bottom-navigation-mobile></bottom-navigation-mobile>
     </v-card>
 
-    <v-dialog v-model="detail"
-        fullscreen
-        hide-overlay
-        transition="dialog-bottom-transition">
+    <v-dialog
+      v-model="detail"
+      fullscreen
+      hide-overlay
+      transition="dialog-bottom-transition"
+    >
       <center-detail @close="detail = false"></center-detail>
     </v-dialog>
   </section>
 </template>
 
 <script>
-import { mapGetters, mapMutations } from "vuex";
+import { mapGetters, mapMutations } from 'vuex'
 
-import CenterDetail from "../components/CenterDetail.vue";
+import CenterDetail from '../components/CenterDetail.vue'
 
-import MapFilter from "../components/maps/MapFilter.vue";
+import MapFilter from '../components/maps/MapFilter.vue'
 
-import BottomNavigationMobile from '../components/BottomNavigation.Mobile.vue';
-import FilterInputs from './filters/FilterInputs.vue';
-import SearchBarMobile from './list/SearchBar.Mobile.vue';
+import BottomNavigationMobile from '../components/BottomNavigation.Mobile.vue'
+import FilterInputs from './filters/FilterInputs.vue'
+import SearchBarMobile from './list/SearchBar.Mobile.vue'
 
 export default {
   components: {
@@ -175,109 +236,109 @@ export default {
   data: () => ({
     loading: true,
     fab: false,
-    search: "",
+    search: '',
     detail: false,
     schedule: null,
     // unavailable: false,
     schedules: [
-      { text: "Entre Semana", value: "week" },
-      { text: "Fines de Semana", value: "weekend" },
+      { text: 'Entre Semana', value: 'week' },
+      { text: 'Fines de Semana', value: 'weekend' },
     ],
     apiKey: process.env.VUE_APP_GOOGLE_MAPS_APIKEY,
   }),
 
   computed: {
     ...mapGetters({
-      _centers: "filtered",
-      getDepartment: "department",
-      getMunicipality: "municipality",
-      getVaccine: "vaccine",
-      getGroup: "group",
-      getDose: "dose",
-      getRequirement: "requirement",
-      getInflux: "influx",
-      getEntrance: "entrance",
+      _centers: 'filtered',
+      getDepartment: 'department',
+      getMunicipality: 'municipality',
+      getVaccine: 'vaccine',
+      getGroup: 'group',
+      getDose: 'dose',
+      getRequirement: 'requirement',
+      getInflux: 'influx',
+      getEntrance: 'entrance',
     }),
 
     department: {
       get() {
-        return this.getDepartment;
+        return this.getDepartment
       },
     },
 
     municipality: {
       get() {
-        return this.getMunicipality;
+        return this.getMunicipality
       },
     },
 
     vaccine: {
       get() {
-        return this.getVaccine;
+        return this.getVaccine
       },
     },
 
     group: {
       get() {
-        return this.getGroup;
+        return this.getGroup
       },
     },
 
     dose: {
       get() {
-        return this.getDose;
+        return this.getDose
       },
     },
 
     requirement: {
       get() {
-        return this.getRequirement;
+        return this.getRequirement
       },
     },
 
     influx: {
       get() {
-        return this.getInflux;
+        return this.getInflux
       },
     },
 
     entrance: {
       get() {
-        return this.getEntrance;
+        return this.getEntrance
       },
     },
 
     centers() {
       return this._centers.filter((center) => {
         switch (this.schedule) {
-          case "week":
-            return !!center.schedule.week;
+          case 'week':
+            return !!center.schedule.week
 
-          case "weekend":
-            return !!center.schedule.weekend;
+          case 'weekend':
+            return !!center.schedule.weekend
 
           default:
-            return true;
+            return true
         }
-      });
+      })
     },
   },
 
   methods: {
     ...mapMutations({
-      setSelected: "SET_SELECTED",
+      setSelected: 'SET_SELECTED',
     }),
 
     formatDose(dose) {
       switch (dose) {
         case dose.match(/AGOTADA/)?.input:
-          return "Agotada";
-        case "PRIMERA":
-          return "1era";
-        case "SEGUNDA":
-          return "2nda";
+          return 'Agotada'
+        case 'PRIMERA':
+          return '1era'
+        case 'SEGUNDA':
+          return '2nda'
         default:
-          return dose;
+          return dose
       }
     },
 
@@ -286,7 +347,7 @@ export default {
         name: 'CenterDetailView',
         params: {
           name: name,
-        }
+        },
       })
     },
 
@@ -294,9 +355,9 @@ export default {
       window.scrollTo({
         top: 0,
         left: 0,
-        behavior: "smooth",
-      });
+        behavior: 'smooth',
+      })
     },
   },
-};
+}
 </script>
