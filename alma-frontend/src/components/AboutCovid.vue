7b4277d4aa16758968467688a7ff13680811c763
Allan Rodriguez <0x.allan@gmail.com>
2022-12-05 19:11:53 -0600

    update home sites

---
+++ alma-frontend/src/components/AboutCovid.vue
@@ -0,0 +1,43 @@
+<template>
+  <div>
+    <v-carousel v-model="model" continuous>
+      <v-carousel-item
+        v-for="(color, i) in colors"
+        :key="color"
+      >
+        <v-sheet
+          :color="color"
+          height="100%"
+          tile
+        >
+          <v-row
+            class="fill-height"
+            align="center"
+            justify="center"
+          >
+            <v-card class="text-h2">
+              <v-card-text>
+                Slide {{ i + 1 }}
+              </v-card-text>
+            </v-card>
+          </v-row>
+        </v-sheet>
+      </v-carousel-item>
+    </v-carousel>
+  </div>
+</template>
+
+<script>
+export default {
+  data: () => ({
+    model: 0,
+    colors: [
+      'primary',
+      'secondary',
+      'yellow darken-2',
+      'red',
+      'orange',
+    ],
+  }),
+}
+</script>
