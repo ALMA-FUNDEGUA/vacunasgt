f29e10b06b4b4804e666d14792b417cd7aeae0c1
Ale Gómez <gom20347@uvg.edu.gt>
2022-12-14 19:41:42 -0600

    changed hyperlink style

--- alma-frontend/src/components/SiteFooter.vue
+++ alma-frontend/src/components/SiteFooter.vue
@@ -40,7 +40,7 @@
 
             &nbsp;
             <span
-              class="footer text-decoration-underline"
+              class="footer"
               style="
                 position: relative;
                 display: flex;
@@ -61,11 +61,17 @@
                   d="M11.742 10.344a6.5 6.5 0 1 0-1.397 1.398h-.001c.03.04.062.078.098.115l3.85 3.85a1 1 0 0 0 1.415-1.414l-3.85-3.85a1.007 1.007 0 0 0-.115-.1zM12 6.5a5.5 5.5 0 1 1-11 0 5.5 5.5 0 0 1 11 0z"
                 />
               </svg>
-              <a @click="$router.push('/filters')"> Busca tu vacuna </a></span
+              <a
+                @click="$router.push('/filters')"
+                style="text-decoration: none"
+                class="footerLinks"
+              >
+                Busca tu vacuna
+              </a></span
             >
             &nbsp;
             <span
-              class="footer text-decoration-underline"
+              class="footer"
               style="
                 position: relative;
                 display: flex;
@@ -85,11 +91,17 @@
                   d="M1 2.828c.885-.37 2.154-.769 3.388-.893 1.33-.134 2.458.063 3.112.752v9.746c-.935-.53-2.12-.603-3.213-.493-1.18.12-2.37.461-3.287.811V2.828zm7.5-.141c.654-.689 1.782-.886 3.112-.752 1.234.124 2.503.523 3.388.893v9.923c-.918-.35-2.107-.692-3.287-.81-1.094-.111-2.278-.039-3.213.492V2.687zM8 1.783C7.015.936 5.587.81 4.287.94c-1.514.153-3.042.672-3.994 1.105A.5.5 0 0 0 0 2.5v11a.5.5 0 0 0 .707.455c.882-.4 2.303-.881 3.68-1.02 1.409-.142 2.59.087 3.223.877a.5.5 0 0 0 .78 0c.633-.79 1.814-1.019 3.222-.877 1.378.139 2.8.62 3.681 1.02A.5.5 0 0 0 16 13.5v-11a.5.5 0 0 0-.293-.455c-.952-.433-2.48-.952-3.994-1.105C10.413.809 8.985.936 8 1.783z"
                 />
               </svg>
-              <a @click="$router.push('/directories')"> Directorios </a></span
+              <a
+                @click="$router.push('/directories')"
+                style="text-decoration: none"
+                class="footerLinks"
+              >
+                Directorios
+              </a></span
             >
             &nbsp;
             <span
-              class="footer text-decoration-underline"
+              class="footer"
               style="
                 position: relative;
                 display: flex;
@@ -113,7 +125,14 @@
                   d="M5.255 5.786a.237.237 0 0 0 .241.247h.825c.138 0 .248-.113.266-.25.09-.656.54-1.134 1.342-1.134.686 0 1.314.343 1.314 1.168 0 .635-.374.927-.965 1.371-.673.489-1.206 1.06-1.168 1.987l.003.217a.25.25 0 0 0 .25.246h.811a.25.25 0 0 0 .25-.25v-.105c0-.718.273-.927 1.01-1.486.609-.463 1.244-.977 1.244-2.056 0-1.511-1.276-2.241-2.673-2.241-1.267 0-2.655.59-2.75 2.286zm1.557 5.763c0 .533.425.927 1.01.927.609 0 1.028-.394 1.028-.927 0-.552-.42-.94-1.029-.94-.584 0-1.009.388-1.009.94z"
                 />
               </svg>
-              <a href="https://docs.almabot.com/" target="blank"> FAQ </a>
+              <a
+                href="https://docs.almabot.com/"
+                target="blank"
+                style="text-decoration: none"
+                class="footerLinks"
+              >
+                FAQ
+              </a>
             </span>
           </div>
         </v-row>
@@ -125,7 +144,7 @@
             href="tel:1-801-00-54321"
             color="#ffe6b7"
             outline="none"
-            style="width: 80%"
+            style="color: #200765; background-color: #d0c2fd; width: 80%"
           >
             <svg
               xmlns="http://www.w3.org/2000/svg"
@@ -153,7 +172,7 @@
             target="_blank"
             color="#ffe6b7"
             outline="none"
-            style="width: 80%"
+            style="color: #200765; background-color: #d0c2fd; width: 80%"
           >
             <svg
               xmlns="http://www.w3.org/2000/svg"
@@ -271,7 +290,7 @@
 </template>
 
 <script>
-import BottomNavigationMobile from '../components/BottomNavigation.Mobile.vue'
+import BottomNavigationMobile from "../components/BottomNavigation.Mobile.vue";
 
 export default {
   components: {
@@ -289,7 +308,7 @@ export default {
       default: false,
     },
   },
-}
+};
 </script>
 
 <style scoped>
@@ -300,6 +319,9 @@ export default {
   padding: 5vh 1.7vh;
 }
 
+.footerLinks:hover {
+  color: #4f4a88;
+}
 .footer-title {
   font-size: 1.2rem;
 }
